4bcf39b8e85922db95b3f63547ac04a7132190a7
Pruthvi Vikram <vikrampruthvi5@gmail.com>
2022-01-18 22:07:04 +0000

    Fixing

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -1,12 +1,6 @@
 module "s3_bucket" {
   source = "terraform-aws-modules/s3-bucket/aws"
-
-  bucket = "my-s3-bucket"
-  acl    = "private"
-  bucket_prefix = var.prefix
-
-  versioning = {
-    enabled = true
-  }
-
+  version = "2.2.0"
+  bucket_prefix="${var.prefix}"
+  bucket = "test-bucket"
 }
